dec6f66d49754ba03c19f83be1503c8d0023c80c
PA Nilsson <pa@excito.com>
2010-01-28 11:09:25 +0000

    * Added update-rc_d function.

--- admin/helpers/exec_helper.php
+++ admin/helpers/exec_helper.php
@@ -37,3 +37,33 @@ function invoke_rc_d( $name, $action ) {
     exec( escapeshellargs( $cmd ), $output, $retval );
     return $retval == 0;
 }
+
+function update_rc_d( $name, $action="defaults", $priority=0, $runlevel=0) {
+		if( ($runlevel != 0) && ($priority != 0) ) {
+		  $cmd = array(
+		      "/usr/sbin/update-rc.d", 
+		      $name, 
+		      $action,
+		      $priority,
+		      $runlevel,
+		      "."
+		  );
+		} else {
+		  if($action == "remove") {
+			  $cmd = array(
+		      "/usr/sbin/update-rc.d", 
+		      "-f", 
+		      $name, 
+		      $action
+			  );
+		  } else {
+			  $cmd = array(
+		      "/usr/sbin/update-rc.d", 
+		      $name, 
+		      $action
+				);
+			}
+		}
+	exec( escapeshellargs( $cmd ), $output, $retval );
+	return $retval == 0;
+}
