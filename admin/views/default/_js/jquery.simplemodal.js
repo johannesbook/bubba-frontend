2c794e74f2600b98fdc775491e9b0fd81fe0a594
PA Nilsson <pa@excito.com>
2010-02-03 11:27:24 +0000

    Fixed "close" buttons in dialogs. Closes #1114

--- admin/views/default/_js/jquery.simplemodal.js
+++ admin/views/default/_js/jquery.simplemodal.js
@@ -275,7 +275,7 @@
 			var self = this;
 
 			// bind the close event to any element with the closeClass class
-			$('.' + this.opts.closeClass).live('click.simplemodal', function (e) {
+			$('.' + this.opts.closeClass).live('click.simplemodal','', function (e) {
 				e.preventDefault();
 				self.close();
 			});
