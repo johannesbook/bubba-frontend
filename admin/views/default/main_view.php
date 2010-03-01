ea903c5505d896e0be38800f689e66b2db40012d
Maximiliam <maxim@excito.com>
2010-03-01 14:14:45 +0000

    Added Jquery ui framework css, modified admin.css and modified man_view.php

--- admin/views/default/main_view.php
+++ admin/views/default/main_view.php
@@ -6,11 +6,9 @@
 <meta http-equiv="Pragma" content="no-cache" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <title>Bubba|TWO - <?=t('title_'.$this->uri->segment(1))?> (<?=php_uname("n")?>)</title>
-<link rel="stylesheet" type="text/css" href="<?=FORMPREFIX.'/views/'.THEME?>/_css/screen.css?v='<?=$this->session->userdata('version')?>'" />
 <link rel="stylesheet" type="text/css" href="<?=FORMPREFIX.'/views/'.THEME?>/_css/admin.css?v='<?=$this->session->userdata('version')?>'" />
-<link rel="stylesheet" type="text/css" href="<?=FORMPREFIX.'/views/'.THEME?>/_css/jquery-ui.css?v='<?=$this->session->userdata('version')?>'" />
+<link rel="stylesheet" type="text/css" href="<?=FORMPREFIX.'/views/'.THEME?>/_css/jquery-ui-default.css?v='<?=$this->session->userdata('version')?>'" />
 <script type="text/javascript" src="<?=FORMPREFIX.'/views/'.THEME?>/_js/jquery.js?v='<?=$this->session->userdata('version')?>'"></script>
-<script type="text/javascript" src="<?=FORMPREFIX.'/views/'.THEME?>/_js/jquery-ui.min.js?v='<?=$this->session->userdata('version')?>'"></script>
 
 <?if(false):?>
 <script type="text/javascript" src="<?=FORMPREFIX.'/views/'.THEME?>/_js/jquery.lint.js?v='<?=$this->session->userdata('version')?>'"></script>
@@ -29,23 +27,13 @@ if(isset($head)) {
 $(document).ready(function(){
 	// Make a striped effect on tables in content
 	$('table:not(.blank)').stripe();
-
-<?if(preg_match("/Opera/i",$_SERVER['HTTP_USER_AGENT'])):?>
-	
-	$("input[type='checkbox']").removeClass("checkbox_radio");
-	$("input[type='radio']").removeClass("checkbox_radio");
-
-<?endif?>
-
-<?if(isset($update) && is_array($update)):?>
-
-	update_status(
-		<?=( isset($update['success']) && $update['success'] ) ? "true" : "false"?>,
-		"<?=isset($update['message']) ? t($update['message']) : ""?>"
-	);
-
-<?endif?>
-
+	<?
+	if(preg_match("/Opera/i",$_SERVER['HTTP_USER_AGENT'])) {
+		// run specific code for Opera here
+		print "$(\"input[type='checkbox']\").removeClass(\"checkbox_radio\");\n";
+		print "$(\"input [type='radio']\").removeClass(\"checkbox_radio\");\n";
+	}
+	?>
 });
 </script>
 </head>
@@ -62,55 +50,26 @@ $(document).ready(function(){
 		}
 	}
 ?>
-
-
-<div id="site_wrapper">
-	<div id="content">
-		<div id="header_wrapper">
-			<div id="header">
-				<div id="logo" style="cursor:pointer;" onclick="location.href='<?=FORMPREFIX?>';"></div>
-				<div id="nav">
-					<?=$navbar?>
-				</div>
-			</div>
-		</div>
-		<div id="site">
-			<div class="segment">
-				<div id="sub_nav">
-					<?=$subnav?>
-				</div>
-				<div class="clear"></div>
-				<?
-				if($topic=$this->uri->segment(1)) {
-					$help_topic = "help_" . $this->uri->segment(2);
-					if(t($help_topic) != $help_topic) {
-						// t-function returned a translated link
-					} else {
-						$help_topic = "help_".$topic;
-					}
-				} else {
-					$topic = "stat";
-					$help_topic = "help_stat";
-				}
-				?>
-				<h1 class="segment_title"><?
-					if(USER!="none"){?>
-						<span class="help"><a href="/manual/<?=t($help_topic)?>" target="_blank"><?=t('Help')?></a></span>
-					<?} else {?>
-						<span class="help"><a href="/manual/<?=t("help_login")?>" target="_blank"><?=t('Help')?></a></span>
-					<?}?>
-					<span class="title"><img class="icon" src="<?=FORMPREFIX.'/views/'.THEME?>/_icons/<?=$topic?>.gif" alt=""></img><?=t('title_'.$topic)?></span></h1>
-				<div class="content">
-					<div class="header">
-						<div class="footer">
-							<?=$content?>
-						</div>
-					</div>
-				</div>
-				<div id="update_status"></div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div id="wrapper">	
+        <div id="header">		
+            <div id="topnav">
+                <span id="topnav_status">Inloggad som Admin</span>
+                <a class="ui-icon ui-icon-lightbulb"></a>
+                <a class="ui-icon ui-icon-home"></a>
+                <a class="ui-icon ui-icon-power"></a>
+                <a id="sideboard_switch" class="ui-icon-carat-1-w"></a>
+            </div>	
+            <a href="#" id="a_logo" onclick="location.href='<?=FORMPREFIX?>';"><img id="img_logo" src="<?=FORMPREFIX.'/views/'.THEME?>/_img/logo.png" alt="BUBBA | 2" title="BUBBA | 2" /></a>
+            <div id="nav"><?=$navbar?></div>
+            <div id="subnav"><?=$subnav?></div>
+        </div>		
+        <div id="content">
+            <?=$content?>				
+        </div>
+        <div id="sideboard" >
+            <img id="sideboard" src="<?=FORMPREFIX.'/views/'.THEME?>/_img/sideboard_tmp.png" alt="tempfil för dashboard" title="tempfil för dashboard" />
+        </div>
+    </div>
+    <div id="update_status"></div>
 </body>
 </html>
