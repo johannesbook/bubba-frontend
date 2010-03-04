0042c218bab93ff45234ef00971dbc0ce74fe6e5
PA Nilsson <pa@excito.com>
2010-03-04 13:26:31 +0000

    Moved "dialog" menu to seperate view file.

--- admin/views/default/main_view.php
+++ admin/views/default/main_view.php
@@ -121,37 +121,6 @@ $(document).ready(function(){
             <img id="img_sideboard" src="<?=FORMPREFIX.'/views/'.THEME?>/_img/sideboard_tmp.png" alt="tempfil för dashboard" title="tempfil för dashboard" />
         </div>
     </div>
-    <div id="home" style="display:none">   
-        <ul>
-            <li>
-                <a class="default-icon default-icon-mail">
-                    <span class="ui-icon ui-icon-locked"></span>
-                </a>    
-                <span class="" style="display:none">Webmail</span>
-            </li>
-            <li>
-                <a class="default-icon default-icon-help"></a>
-                <span class="" style="display:none">Help</span>
-            </li>
-            <li>
-                <a class="default-icon default-icon-filemanager">
-                    <span class="ui-icon ui-icon-locked"></span>
-                </a>                
-                <span class="" style="display:none">Help</span>
-            </li>
-            <li>
-                <a class="default-icon default-icon-music">
-                    <span class="ui-icon ui-icon-locked"></span>
-                </a>
-                <span class="" style="display:none">Music</span>
-            </li>
-            <li>
-                <a class="default-icon default-icon-logout"></a>
-                <span class="" style="display:none">Logout</span>
-            </li>            
-        </ul>
-        <a id="home_close" class="ui-icon ui-icon-closethick" onclick="$('#home').toggle()"></a>
-    </div>
     <div id="update_status"></div>
 </body>
 </html>
---
+++ admin/views/default/menu_view.php
@@ -0,0 +1,31 @@
+    <div id="home" style="display:none">   
+        <ul>
+            <li>
+                <a class="default-icon default-icon-mail">
+                    <span class="ui-icon ui-icon-locked"></span>
+                </a>    
+                <span class="" style="display:none">Webmail</span>
+            </li>
+            <li>
+                <a class="default-icon default-icon-help"></a>
+                <span class="" style="display:none">Help</span>
+            </li>
+            <li>
+                <a class="default-icon default-icon-filemanager">
+                    <span class="ui-icon ui-icon-locked"></span>
+                </a>                
+                <span class="" style="display:none">Help</span>
+            </li>
+            <li>
+                <a class="default-icon default-icon-music">
+                    <span class="ui-icon ui-icon-locked"></span>
+                </a>
+                <span class="" style="display:none">Music</span>
+            </li>
+            <li>
+                <a class="default-icon default-icon-logout"></a>
+                <span class="" style="display:none">Logout</span>
+            </li>            
+        </ul>
+        <a id="home_close" class="ui-icon ui-icon-closethick" onclick="$('#home').toggle()"></a>
+    </div>
