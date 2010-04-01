cdff3cbb3f3c9de75e66a9aafd7325f2f8e40c9d
Tor Krill <tor@xcito.com>
2010-04-01 09:54:31 +0000

    Add special handling for IE filenames in download

--- admin/helpers/content_disposition_helper.php
+++ admin/helpers/content_disposition_helper.php
@@ -6,6 +6,7 @@ function content_disposition( $type, $filename ) {
 	$CI = &get_instance();
 	$CI->load->library('browser_cap');
 	$browser = $CI->browser_cap->getBrowser();
+
 	if( 
 		($browser->Browser == 'Firefox' && $browser->MajorVer >= 3)
 		||
@@ -23,10 +24,17 @@ function content_disposition( $type, $filename ) {
 		// Firefox and opera also seems to do this, but we uses RFC 2231 for them
 		
 		return "Content-Disposition: $type; filename=\"".mb_encode_mimeheader($filename, 'UTF-8', 'Q')."\"";
+	} elseif(
+		($browser->Browser == 'IE' && $browser->MajorVer >= 7)
+	){
+	
+		return "Content-Disposition: $type; filename=".rawurlencode($filename);
+
 	} else {
 
 		// Poor MSIE gets the bad name, nothing to do sadly, as it doesn't support standards
 		// Safari goes here too as it seems to lack finess as well.
 		return "Content-Disposition: $type; filename=\"".$filename."\"";
+
 	}
 }
