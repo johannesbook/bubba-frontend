84c2ae425d1229e207178f3a1ad8c50afc169dfb
Carl Fürstenberg <carl@excito.com>
2011-03-23 11:27:55 +0000

    * Adding bubba-kernel to the list of installed packages (Closes: 1616) * Adding bubba package to list of installed packages, chaning session   userdata to use that version isntead of bubba-frontend version.

--- admin/controllers/settings.php
+++ admin/controllers/settings.php
@@ -245,8 +245,8 @@ class Settings extends Controller{
 			$output = apt_query_progress();
 			break;
 		case 'get_versions':
-			$versions = get_package_version(array("bubba-frontend","bubba-backend","bubba-album","filetransferdaemon","squeezecenter"));
-			$this->session->set_userdata("version",$versions['bubba-frontend']);
+			$versions = get_package_version(array("bubba","bubba-kernel","bubba-frontend","bubba-backend","bubba-album","filetransferdaemon","squeezecenter"));
+			$this->session->set_userdata("version",$versions['bubba']);
 			$output = json_encode($versions);
 			break;
 		}
