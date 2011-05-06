b474b413593fb3b006235112b085f16df37e66ab
Carl Fürstenberg <carl@excito.com>
2011-05-06 14:28:03 +0200

    move update backup jobs table ajax call inside the function, and call it as well when create job is done

--- admin/views/default/_js/backup.js
+++ admin/views/default/_js/backup.js
@@ -85,59 +85,59 @@ $(function(){
         });
     }
 
-    var update_backup_jobs_table = function(jobs) {
-        var table = $("#fn-backup-jobs tbody");
-        table.empty();
-        var row = $("<tr/>");
-        $.each( jobs, function() {
-            var data = $.extend({failed: false, running: false},this);
-            var cur = row.clone();
-            cur.appendTo(table).
-            append($('<td/>',{text: data.name})).
-            append($('<td/>',{text: data.target})).
-            append($('<td/>',{text: data.schedule})).
-            append($('<td/>',{text: data.status}).toggleClass("ui-backup-job-failed", data.failed)).
-            append($('<td/>').append($("<span/>", {'class': 'ui-inline'}).append(
-                $('<button/>', {
-                    'class' : "submit",
-                    html: $.message('backup-job-remove-button-label'),
-                    click: function(){}
-                }),
-                $('<button/>', {
-                    'class' : "submit",
-                    html: $.message('button-label-edit'),
-                    click: function(){}
-                }),
-                $('<button/>', {
-                    'class' : "submit",
-                    html: $.message('backup-job-run-now-button-label'),
-                    click: function(){}
-                }))
-                )
-                ).
-            click(function(){
-                $.post(
-                    config.prefix + "/ajax_backup/get_backup_job_information",
-                    {name: data.name},
-                    function(data) {
-                        update_backup_job_information(data);
-                    },
-                    "json"
-                );
+    var update_backup_jobs_table = function() {
+        $.post( config.prefix + "/ajax_backup/get_backup_jobs", {},
+            function(jobs) {
+                var table = $("#fn-backup-jobs tbody");
+                table.empty();
+                var row = $("<tr/>");
+                $.each( jobs, function() {
+                    var data = $.extend({failed: false, running: false},this);
+                    var cur = row.clone();
+                    cur.appendTo(table).
+                    append($('<td/>',{text: data.name})).
+                    append($('<td/>',{text: data.target})).
+                    append($('<td/>',{text: data.schedule})).
+                    append($('<td/>',{text: data.status}).toggleClass("ui-backup-job-failed", data.failed)).
+                    append($('<td/>').append($("<span/>", {'class': 'ui-inline'}).append(
+                        $('<button/>', {
+                            'class' : "submit",
+                            html: $.message('backup-job-remove-button-label'),
+                            click: function(){}
+                        }),
+                        $('<button/>', {
+                            'class' : "submit",
+                            html: $.message('button-label-edit'),
+                            click: function(){}
+                        }),
+                        $('<button/>', {
+                            'class' : "submit",
+                            html: $.message('backup-job-run-now-button-label'),
+                            click: function(){}
+                        }))
+                        )
+                        ).
+                    click(function(){
+                        $.post(
+                            config.prefix + "/ajax_backup/get_backup_job_information",
+                            {name: data.name},
+                            function(data) {
+                                update_backup_job_information(data);
+                            },
+                            "json"
+                        );
 
-            });
-            cur.find("button").attr('disabled', data.running ? 'disabled' : '').
-            toggleClass('disabled', data.running)
+                    });
+                    cur.find("button").attr('disabled', data.running ? 'disabled' : '').
+                    toggleClass('disabled', data.running)
 
-        }
-    );
+                }
+            );
+            },
+            "json"
+        );
     }
-    $.post( config.prefix + "/ajax_backup/get_backup_jobs", {},
-        function(data) {
-            update_backup_jobs_table(data);
-        },
-        "json"
-    );
+    update_backup_jobs_table();
     $.each(['create'], function(index, value) {
 
         if (typeof dialog_options[value] == "undefined") {
@@ -299,6 +299,7 @@ $(function(){
             'success': function( data ) {
                 $.throbber.hide();
                 dialogs['create'].dialog('close');
+                update_backup_jobs_table();
             }
         }
     );
